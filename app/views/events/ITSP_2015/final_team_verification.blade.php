1969fdf1655a4047d4514dd50fa6aaa11147d445
Anay Tripathi <tripathi.anay@gmail.com>
2015-06-16 09:47:58 +0530

    First commit by anay. Added hall of fame.

--- app/views/events/ITSP_2015/final_team_verification.blade.php
+++ app/views/events/ITSP_2015/final_team_verification.blade.php
@@ -12,6 +12,6 @@
 <h3>Information given in this form will be treated as the final information about your ITSP project and the same will get printed on the certificates.</h3>
 <iframe src="https://docs.google.com/forms/d/1NbBw3hwpNjLnb5IbjQvBiEj6QrOKHN5M4i8E2g_sQAY/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 @else
-<h3> Form is removed.</h3>
+<h3>Form is removed.</h3>
 @endif
 @endsection
